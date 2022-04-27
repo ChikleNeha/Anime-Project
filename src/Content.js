d62cb83326d019fde4cfffc8a79f8f8d9ff9e386
ChikleNeha <chikleneha473@gmail.com>
2022-04-27 13:01:19 +0530

    2 files modified, 1 added

---
+++ src/Content.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Content(){
+  return (
+    <div className="content">
+       <img id="img-poster" src="https://i.pinimg.com/originals/1a/82/e1/1a82e13747ed52a173d894f55465fca5.jpg" alt="poster" width="250px"/>
+       <div className="content-info">
+         <h1>I Want To Eat Your Pancreas</h1>
+         <p>Original title : Kimi no suizô o tabetai</p>
+         <span id="date">2018 . Not Rated . 1h 49m</span>
+         <p id="description">A high school student discovers one of his classmates, Sakura Yamauchi, is suffering from a terminal illness. This secret brings the two together, as she lives out her final moments.</p>
+         <p id="director"> <b>Director : </b> Shin'ichirô Ushijima</p>
+      </div>
+       
+    </div>
+  )
+}
--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Content from "./Content"
 
 export default function Movie(){
   return (
-    <div className="movie">
-      
+   
+    <div>
+        <div className="movie">
+            <Content />
+        </div>
+
+        <div>
+          <button  id="buttons">Back</button>
+          <button  id="buttons">Next</button>
+        </div>
     </div>
   )
 }
